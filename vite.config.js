a1c47edc4c4c6e625a7c92e450ce9596eadde826
Restyled.io <commits@restyled.io>
2024-10-07 14:22:34 +0000

    Restyled by clang-format

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path"
+import {defineConfig} from "vite"
 
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/AI_Trip_Planner/' : '/',
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+  plugins : [ react() ],
+  base : process.env.NODE_ENV === 'production' ? '/AI_Trip_Planner/' : '/',
+  resolve : {
+    alias : {
+      "@" : path.resolve(__dirname, "./src"),
     },
   },
 })
