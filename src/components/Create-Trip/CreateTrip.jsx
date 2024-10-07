d7a454413409d73eba7211e25b4771fcffdd1dc8
siddhesh2017 <siddheshjagtap2017@gmail.com>
2024-10-07 19:49:51 +0530

    Added configs for deployment (Github Pages)

--- src/components/Create-Trip/CreateTrip.jsx
+++ src/components/Create-Trip/CreateTrip.jsx
@@ -3,6 +3,7 @@ import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { Input } from "@/components/ui/input";
 import { SelectTravelerList, SelectBudgetOptions } from '../../constants/options';
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 
 const CreateTrip = () => {
@@ -18,6 +19,11 @@ const CreateTrip = () => {
     useEffect(() => {
         console.log(formData);
     }, [formData]);
+
+    const onGenerateTrip = () =>{
+        console.log("Trip generated");
+    }
+
     return (
         <>
             <div className="bg-slate-200 w-full h-auto py-10  px-40">
@@ -43,7 +49,7 @@ const CreateTrip = () => {
                     <div>
                         <h2 className='text-xl mb-2'>How many days are you planning your trip?</h2>
                         <Input type="number"  onChange={(e) => {
-                            (e.target.value > 10 || e.target.value < 1)? alert("We plan trip for maximum 10 days only, Please select between 1 - 10") : handleFormData('noOfDays', e.target.value);
+                            (e.target.value > 10 || e.target.value < 1)? toast("We plan trip for maximum 10 days only!!") : handleFormData('noOfDays', e.target.value);
                         }} />
                     </div>
 
@@ -82,7 +88,7 @@ const CreateTrip = () => {
 
                 </div>
                             {/* <Button className="mt-10" >Generate Trip</Button> */}
-                            <div className='my-10 w-full flex justify-end'>
+                            <div onClick={onGenerateTrip} className='my-10 w-full flex justify-end'>
                                 <button
                                     className="cursor-pointer text-white font-bold relative text-[14px] w-[9em] h-[3em] text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s]  active:bg-violet-700 focus:ring-violet-700"
                                     >
