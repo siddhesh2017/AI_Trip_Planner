4016b2d06884d390482018f4b8d3b7b102f8d3a6
siddhesh2017 <siddheshjagtap2017@gmail.com>
2024-10-10 16:48:05 +0530

    Saved trips using Google firebase

--- src/components/Create-Trip/CreateTrip.jsx
+++ src/components/Create-Trip/CreateTrip.jsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 import { SelectTravelerList, SelectBudgetOptions } from '../../constants/options';
 import { toast } from "sonner";
 import { AI_PROMPT } from '../../constants/options';
-import { chatSession } from '@/Gemini_AI_service/AIModel';
+import { chatSession } from '@/services/AIModel';
 import {
     Dialog,
     DialogContent,
@@ -16,6 +16,8 @@ import {
 import { Button } from '../ui/button';
 import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+import { db } from '@/services/FireBaseConfig';
+import { doc, setDoc } from "firebase/firestore"; 
 
   
 
@@ -23,6 +25,7 @@ const CreateTrip = () => {
     const [place, setPlace] = useState();
     const [formData, setFormData] = useState([]);
     const [openDialog, setOpenDialog] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     const handleFormData = (name, value) =>{
         setFormData({
@@ -45,16 +48,31 @@ const CreateTrip = () => {
         if(!formData?.location || !formData.noOfDays || !formData.budget || !formData.noOfPeople){
             toast.error("Please fill all the fields!!");
         }
+        setLoading(true);
         const Final_Prompt = AI_PROMPT
         .replace('{location}    ', formData?.location?.label)
         .replace('{totalDays}', formData?.noOfDays)
         .replace('{traveler}', formData?.noOfPeople)
         .replace('{budget}', formData?.budget)
         .replace('{totalDays}', formData?.noOfDays);
-        console.log(Final_Prompt);
-
+        // console.log(Final_Prompt);
         const result = await chatSession.sendMessage(Final_Prompt);
         console.log(result?.response?.text());
+        // clearFormData();
+        toast.success("Trip generated successfully!!");
+        saveTrip(result?.response?.text());
+    }
+    const saveTrip = async (TripData) => {
+        const user = JSON.parse(localStorage.getItem('user'));
+        const docId = Date.now().toString();
+        await setDoc(doc(db, "AItrips", docId), {
+            id: docId,
+            userInput: formData,
+            tripData: JSON.parse(TripData),
+            userEmail: user?.email
+          });
+          setLoading(false);
+          console.log("Trip saved successfully!!");
     }
     const GetUserProfile = (tokenInfo) => {
         try{
@@ -64,8 +82,13 @@ const CreateTrip = () => {
                     Accept:'Application/json'
                 }
             }).then((resp) => {
+                toast("Logged in successfully!!");
                 console.log(resp);
                 localStorage.setItem('user', JSON.stringify(resp?.data));
+                // setTimeout(() => {
+                //     localStorage.removeItem('user');
+                //     toast('Session expired!');
+                // }, 1000000);
                 setOpenDialog(false);
                 onGenerateTrip();
             })
@@ -84,7 +107,7 @@ const CreateTrip = () => {
 
     return (
         <>
-            <div className="bg-gradient-to-br from-purple-100 via-orange-100 to-yellow-100 w-full h-auto py-10  px-40 pt-28">
+            <div className="bg-gradient-to-br from-purple-100 via-orange-100 to-yellow-100 w-full h-auto py-10 px-10 sm:px-20 md:px-40 lg:px-80  pt-28">
                 <h2 className="text-4xl">Tell us your travel preferences</h2>
                 <p className="font-normal font-neutral-400 mt-1">Just provide some basic information and our trip planner will generate a customized itinerary based on your preferences.</p>
 
@@ -120,7 +143,7 @@ const CreateTrip = () => {
                                     <div onClick={() => {
                                             handleFormData('budget', item.title);
                                         }} key={index+(Math.random()*10)} 
-                                        className={`p-4 cursor-pointer border border-transparent bg-clip-padding rounded-lg w-full bg-white     shadow-lg hover:shadow-xl ${(item.title == formData.budget) && "bg-zinc-100"}`} >
+                                        className={`p-4 cursor-pointer border border-transparent bg-clip-padding rounded-lg w-full h-full bg-white     shadow-lg hover:shadow-xl ${(item.title == formData.budget) && "bg-zinc-100"}`} >
                                             <h2 className='text-3xl'>{item.icon}</h2>
                                             <h2 className='mt-1 font-bold text-lg'>{item.title}</h2>
                                             <h2 className='font-normal text-sm'>{item.desc}</h2>
@@ -139,7 +162,7 @@ const CreateTrip = () => {
                                     <div onClick={() => {
                                         handleFormData('noOfPeople', item.title);
                                     }}
-                                    key={index+(Math.random()*10)} className={`p-4 cursor-pointer border rounded-lg bg-white hover:shadow-lg ${(item.title == formData.noOfPeople) && "bg-zinc-100"} `} >
+                                    key={index+(Math.random()*10)} className={`p-4 cursor-pointer w-full h-full border rounded-lg bg-white hover:shadow-lg ${(item.title == formData.noOfPeople) && "bg-zinc-100"} `} >
                                         <h2 className='text-3xl'>{item.icon}</h2>
                                         <h2 className='mt-1 font-bold text-lg'>{item.title}</h2>
                                         <h2 className='font-normal text-sm'>{item.desc}</h2>
@@ -151,14 +174,14 @@ const CreateTrip = () => {
 
                 </div>
                             {/* <Button className="mt-10" >Generate Trip</Button> */}
-                            <div className='my-10 w-full flex justify-end items-center gap-10'>
+                            <div className='my-20 w-full flex justify-start items-center gap-10'>
                                 <button
                                     onClick={clearFormData}
-                                    class="group relative flex h-10 w-10 flex-col items-center justify-center overflow-hidden rounded-lg  bg-red-500 hover:bg-red-600 focus:shadow-md focus:shadow-black"
+                                    class="group relative flex h-10 w-10 flex-col items-center justify-center overflow-hidden rounded-lg  bg-red-600 hover:bg-red-500 focus:shadow-md focus:shadow-black"
                                     >
                                     <svg
                                         viewBox="0 0 1.625 1.625"
-                                        class="absolute -top-7 fill-white delay-100 group-hover:top-6 group-hover:animate-[spin_1.4s] group-hover:duration-1000"
+                                        className="absolute -top-7 fill-white delay-100 group-hover:top-3 group-hover:animate-[spin_1.4s] group-hover:duration-1000"
                                         height="15"
                                         width="15"
                                     >
@@ -176,7 +199,7 @@ const CreateTrip = () => {
                                         width="14"
                                         fill="none"
                                         viewBox="0 0 39 7"
-                                        class="origin-right duration-500 group-hover:rotate-90"
+                                        className="origin-right duration-500 group-hover:rotate-90"
                                     >
                                         <line stroke-width="4" stroke="white" y2="5" x2="39" y1="5"></line>
                                         <line
@@ -188,7 +211,7 @@ const CreateTrip = () => {
                                         x1="12"
                                         ></line>
                                     </svg>
-                                    <svg width="13" fill="none" viewBox="0 0 33 39" class="">
+                                    <svg width="13" fill="none" viewBox="0 0 33 39" className="">
                                         <mask fill="white" id="path-1-inside-1_8_19">
                                         <path
                                             d="M0 0H33V35C33 37.2091 31.2091 39 29 39H4C1.79086 39 0 37.2091 0 35V0Z"
@@ -206,11 +229,46 @@ const CreateTrip = () => {
 
                                 <button 
                                     onClick={onGenerateTrip}
-                                    className="cursor-pointer text-white font-bold relative text-[14px] w-[9em] h-[3em] text-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-xl before:transition-all before:ease-in-out before:duration-[1s]  active:bg-violet-700 focus:ring-violet-700"
+                                    className="cursor-pointer text-white font-bold relative text-[14px] w-[9em] h-[3em] text-center flex items-center justify-center bg-gradient-to-r from-violet-500 from-10% via-sky-500 via-30% to-pink-500 to-90% bg-[length:400%] rounded-[30px] z-10 hover:animate-gradient-xy hover:bg-[length:100%] before:content-[''] before:absolute before:-top-[5px] before:-bottom-[5px] before:-left-[5px] before:-right-[5px] before:bg-gradient-to-r before:from-violet-500 before:from-10% before:via-sky-500 before:via-30% before:to-pink-500 before:bg-[length:400%] before:-z-10 before:rounded-[35px] before:hover:blur-sm before:transition-all before:ease-in-out before:duration-[1s]  active:bg-violet-700 focus:ring-violet-700"
                                     >
-                                    Generate Trip
+                                        {loading ? 
+                                            
+                                            <>
+                                                <div class='flex space-x-2 justify-center items-center bg-transperant h-auto dark:invert'>
+                                                    <span class='sr-only'>Loading...</span>
+                                                    <div class='h-3 w-3 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+                                                    <div class='h-3 w-3 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+                                                    <div class='h-3 w-3 bg-white rounded-full animate-bounce'></div>
+                                                </div>
+                                            </>
+
+                                        : 
+                                            <>
+                                                Generate Trip
+                                            </>
+                                        }
+                                    
                                 </button>
                             </div>
+
+                            {/* <Dialog open={loading}>
+                                <DialogContent>
+                                    <DialogHeader>
+                                    <DialogTitle>
+                                        <div className="flex-col gap-4 w-full flex items-center justify-center">
+                                            <div
+                                                className="w-20 h-20 border-4 border-transparent text-blue-400 text-4xl animate-spin flex items-center justify-center border-t-blue-400 rounded-full"
+                                            >
+                                                <div
+                                                className="w-16 h-16 border-4 border-transparent text-red-400 text-2xl animate-spin flex items-center justify-center border-t-red-400 rounded-full"
+                                                ></div>
+                                            </div>
+                                        </div>
+                                        <h1>Loading..</h1>
+                                    </DialogTitle>
+                                    </DialogHeader>
+                                </DialogContent>
+                            </Dialog> */}
 
                             <Dialog open={openDialog}>
                                 <DialogContent>
@@ -225,16 +283,29 @@ const CreateTrip = () => {
                                         <h2 className='font-bold text-xl mt-3 flex items-center gap-1'>Sign in with Google</h2>
                                         <p>Sign in to the App with Google Authentication securely</p>
 
-                                        <Button className='w-full mt-5 flex items-center gap-2' onClick={login}>
-                                            <img width="25" height="25" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo"/>
-                                            Sign in with Google
+                                        <Button  className='w-full mt-5 flex items-center gap-2' onClick={login}>
+                                            {loading ? 
+                                            
+                                                <>
+                                                    <div class='flex space-x-2 justify-center items-center bg-transperant h-auto dark:invert'>
+                                                        <span class='sr-only'>Loading...</span>
+                                                        <div class='h-3 w-3 bg-white rounded-full animate-bounce [animation-delay:-0.3s]'></div>
+                                                        <div class='h-3 w-3 bg-white rounded-full animate-bounce [animation-delay:-0.15s]'></div>
+                                                        <div class='h-3 w-3 bg-white rounded-full animate-bounce'></div>
+                                                    </div>
+                                                </>
+
+                                            : 
+                                                <>
+                                                    <img width="25" height="25" src="https://img.icons8.com/fluency/48/google-logo.png" alt="google-logo"/>
+                                                    Sign in with Google
+                                                </>
+                                            }
                                         </Button>
                                     </DialogDescription>
                                     </DialogHeader>
                                 </DialogContent>
                             </Dialog>
-
-
             </div>
         </>
     )
